09fc048a138bad0b41c6bbe5ff932a537097006c
BoZhaoUTSC <bodotzhao@gmail.com>
2018-01-26 16:18:42 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express     = require('express'),
     path        = require('path'),
     APP_DIR     = path.join(__dirname, '/dist'),
-    app         = express();
+    app         = express(),
+    request     = require('request');
 
 
 app.use(express.static(APP_DIR));
@@ -9,8 +10,23 @@ app.use(express.static(APP_DIR));
 app.get('/convert', (req, res) => {
     res.send('Hello World!');
     console.log(req);
-    console.log("------------" + new Date() + "------------\n");
+    console.log("------------" + new Date() + "-----GET--------\n");
 })
+
+app.post('/convert', (req, res) => {
+    res.send('Hello World!');
+    console.log(req);
+    console.log("------------" + new Date() + "-----POST-------\n");
+})
+
+// test an endpoint
+// app.get('/test', (req, res) => {
+//     request('http://localhost:3000/convert', { json: true }, (err, res, body) => {
+//         if (err) { return console.log(err); }
+//         res.send("hit /test")
+//     }); 
+// })
+
 
 app.listen(3000, () => {
     console.log('App is listening on port 3000.')
